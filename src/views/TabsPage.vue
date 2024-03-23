4c1bb84527c26090d35b60f4e36eb55f9fec39f9
Vokhidjon12 <vipkeyl001@gmail.com>
2024-03-23 15:29:08 +0500

    first

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,13 +2,36 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+      <ion-buttons slot="bottom">
+        <ion-button
+          tab="tab1"
+          router-link="/tabs/tab1"
+          routerDirection="forward"
+        >
+          <!-- <ion-icon :icon="timeOutLine" size="large" /> -->
           <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
+        </ion-button>
+        <ion-button
+          tab="tab2"
+          router-link="/tabs/tab2"
+          routerDirection="forward"
+          color="dark"
+        >
+          <!-- <ion-icon :icon="timeOutLine" size="large" ><ion-icon /> -->
+          <ion-label>Tab 2</ion-label>
+        </ion-button>
+        <ion-button
+          tab="tab3"
+          router-link="/tabs/tab3"
+          routerDirection="forward"
+          color="dark"
+        >
+          <!-- <ion-icon :icon="timeOutLine" size="large" ><ion-icon /> -->
+          <ion-label>Tab 2</ion-label>
+        </ion-button>
+      
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <!-- <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
@@ -16,13 +39,38 @@
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
+        </ion-tab-button> -->
+      </ion-buttons>
     </ion-tabs>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+<script lang="ts">
 import { ellipse, square, triangle } from 'ionicons/icons';
+
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+  IonButton,
+  IonNavLink,
+} from '@ionic/vue';
+
+export default {
+  components: {
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
+    IonLabel,
+    IonIcon,
+    IonPage,
+    IonRouterOutlet,
+    IonButton,
+    IonNavLink,
+  },
+};
 </script>
